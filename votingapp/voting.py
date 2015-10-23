9cc65078efc9990f54d658da1de284fecde49a9d
FedeScuote <fscuoteguazza@gmail.com>
2015-10-23 09:08:01 -0300

    arregle errores pep8

--- votingapp/voting.py
+++ votingapp/voting.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, json, request
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template('login.html')
@@ -8,16 +9,15 @@ def index():
 if __name__ == "__main__":
     app.run()
 
-@app.route('/login',methods=['POST'])
-def signUp():
 
+@app.route('/login', methods=['POST'])
+def signUp():
 
     _serie = request.form['credSerie']
     _numero = request.form['credNumero']
 
-
     # validate the received values
     if _serie and _numero:
-        return json.dumps({'html':'<span>Usuario valido</span>'})
+        return json.dumps({'html': '<span>Usuario valido</span>'})
     else:
-        return json.dumps({'html':'<span>Ingrese los valores en los campos requeridos, por favor</span>'})
+        return json.dumps({'html': '<span>Ingrese los valores en los campos requeridos, por favor</span>'})

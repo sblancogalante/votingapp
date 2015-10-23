82f62d2d75dbe3b3528f65523c3767298d8dc914
Santiago Blanco <santiagoblanco@192.168.1.3>
2015-10-22 21:17:03 -0300

    Fixed html file

--- votingapp/voting.py
+++ votingapp/voting.py
@@ -8,3 +8,16 @@ def index():
 if __name__ == "__main__":
     app.run()
 
+@app.route('/login',methods=['POST'])
+def signUp():
+
+    # read the posted values from the UI
+    _serie = request.form['inputName']
+    _numero = request.form['inputEmail']
+
+
+    # validate the received values
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})

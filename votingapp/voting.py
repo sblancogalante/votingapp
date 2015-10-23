33e43e42c5e501eacd7a13ff703f02b751fccf59
Santiago Blanco <santiagoblanco@192.168.1.3>
2015-10-22 21:31:49 -0300

    Fixed html file and first try of login event method. Still missing the event trigger

--- votingapp/voting.py
+++ votingapp/voting.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,13 +11,13 @@ if __name__ == "__main__":
 @app.route('/login',methods=['POST'])
 def signUp():
 
-    # read the posted values from the UI
-    _serie = request.form['inputName']
-    _numero = request.form['inputEmail']
+
+    _serie = request.form['credSerie']
+    _numero = request.form['credNumero']
 
 
     # validate the received values
-    if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields good !!</span>'})
+    if _serie and _numero:
+        return json.dumps({'html':'<span>Usuario valido</span>'})
     else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
+        return json.dumps({'html':'<span>Ingrese los valores en los campos requeridos, por favor</span>'})

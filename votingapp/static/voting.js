4072f56c5b7014f0162fa393e5dc8b299d2826b1
FedeScuote <fscuoteguazza@gmail.com>
2015-11-16 23:29:36 -0300

    should insert without problems

--- votingapp/static/voting.js
+++ votingapp/static/voting.js
@@ -6,8 +6,36 @@ votingApp.controller('votingController', ['$scope', '$http', '$window', function
       var loginUrl = '/login';
       var request = $http.post(loginUrl, {'serie': voting.serie, 'numero': voting.numero});
       request.success(function(){
-          $window.location.href = '/caca';
+          $window.location.href = '/vote';
       });
 }
+  voting.vote1 = function() {
+      var voteUrl ='/vote'
+      var request = $http.post(voteUrl, {'candidato': 1});
+      request.success(function(){
+          alert('su voto fue registrado!');
+          $window.location.href = '/';
+      });
+
+
+  };
+  voting.vote2 = function() {
+      var voteUrl ='/vote'
+      var request = $http.post(voteUrl, {'candidato': 2});
+      request.success(function(){
+          alert('su voto fue registrado!');
+          $window.location.href = '/';
+      });
+
+
+  };
+  voting.voteBlank = function() {
+      var voteUrl = '/vote'
+      var request = $http.post(voteUrl, {'voto en blanco': '1'});
+      request.success(function(){
+          alert('su voto fue registrado!');
+          $window.location.href = '/';
+      });
+  }
 }
 ]);
--- votingapp/voting.py
+++ votingapp/voting.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 mysql = MySQL()
 # MySQL configurations
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'votacionesDB'
 app.config['MYSQL_HOST'] = 'localhost'
 mysql.init_app(app)
@@ -24,13 +24,32 @@ def sign_up():
     numero = data.get('numero')
     # # validate the received values
     if serie and numero:
-        cursor.execute("""select votantes.doc_serie, votantes.doc_num from votantes
-        where votantes.doc_serie = %sserie and votantes.doc_num = %snumero;""", (serie, numero))
-        print(cursor.fetchall())
+        query = """select votantes.doc_serie, votantes.doc_num from votantes
+        where votantes.doc_serie = %s and votantes.doc_num = %s;"""
+        cursor.execute(query, (serie, numero))
         return json.dumps(str(cursor.fetchall()))
     else:
         return json.dumps('false')
 
 
+@app.route('/vote', methods=['POST', 'GET'])
+def vote():
+    if request.method == 'GET':
+        return render_template('voting.html')
+    else:
+        try:
+            cursor = mysql.connection.cursor()
+            data = json.loads(request.data)
+            candidato = data.get('candidato')
+            if candidato:
+                if candidato == 1:
+                    query = """insert into votos(opcion_voto, observado) values(1,0)"""
+                if candidato == 2:
+                    query = """insert into votos(opcion_voto, observado) values(2,0)"""
+            else:
+                query = """insert into votos(opcion_voto, observado) values(null,0)"""
+            cursor.execute(query)
+        except Exception as error:
+            print(error)
 if __name__ == "__main__":
     app.run()
